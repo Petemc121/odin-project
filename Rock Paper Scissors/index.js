d8c400ba07b9576032c7f050e56c47fb2168eb93
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-05-15 23:27:17 +0900

    Update index.js
    Added rounds and winner declare to rps

--- Rock Paper Scissors/index.js
+++ Rock Paper Scissors/index.js
@@ -1,71 +1,77 @@
-
-let userInput; 
-let computerInput;
-const play = document.getElementById('play');
 rpsArray = ["rock", "paper", "scissors"];
+let round = 1;
+let playerWins = 0;
+let computerWins = 0;
 
-play.addEventListener('click', function() {
-
- userInput = prompt("Enter rock, paper or sciccors.");
-
- if (rpsArray.includes(userInput.toLowerCase()) !== undefined)
- {
-      computerInput = rpsArray[Math.floor(Math.random() * 2)];
- } else 
- {
-   alert('please enter a valid input');
- }
-
-  playRound(userInput, computerInput);
-});
-
-
-function playRound(playerSelection, computerInput)
-{
-     if (playerSelection === computerInput)
-     {
-        alert(computerInput + ": tie!")
-     } else 
-     {
-       if (playerSelection === "rock")
-       {
-         if (computerInput === "paper")
-         {
-           alert(computerInput + ": Computer wins!")
-         }
-
-         if (computerInput === "scissors")
-         {
-           alert(computerInput + ": You win!")
-         }
-       }
-
-       if (playerSelection === "scissors")
-       {
-         if (computerInput === "paper")
-         {
-           alert(computerInput + ": You win!")
-         }
-
-         if (computerInput === "rock")
-         {
-           alert(computerInput + ": Computer wins!")
-         }
-        }
-
-         if (playerSelection === "paper")
-         {
-           if (computerInput === "scissors")
-           {
-             alert(computerInput + ": Computer wins!")
-           }
-  
-           if (computerInput === "rock")
-           {
-             alert(computerInput + ": You win!")
-           }
-       }
-     }
+function play() {
+  let userInput = prompt("Enter rock, paper or sciccors.");
+  if (rpsArray.includes(userInput.toLowerCase()) !== false) {
+    computerInput = computerPlay();
+    console.log(playRound(userInput, computerInput, round));
+  } else {
+    alert("please enter a valid input");
+  }
 }
 
+function game() {
+  for (let i = 0; i < 5; i++) {
+    play();
+    round++;
+  }
 
+  if (computerWins > playerWins) {
+    return "Winner: computer";
+  } else if (playerWins > computerWins) {
+    return "Winner: player";
+  } else {
+    return "Tie!";
+  }
+}
+
+function computerPlay() {
+  return rpsArray[Math.floor(Math.random() * 2)];
+}
+
+function playRound(playerSelection, computerInput, round) {
+  if (playerSelection === computerInput) {
+    return "round: " + round + " " + computerInput + ": tie!";
+  } else {
+    if (playerSelection === "rock") {
+      if (computerInput === "paper") {
+        computerWins++;
+        return "round: " + round + " " + computerInput + ": Computer wins!";
+      }
+
+      if (computerInput === "scissors") {
+        playerWins++;
+        return "round: " + round + " " + computerInput + ": You win!";
+      }
+    }
+
+    if (playerSelection === "scissors") {
+      if (computerInput === "paper") {
+        playerWins++;
+        return "round: " + round + " " + computerInput + ": You win!";
+      }
+
+      if (computerInput === "rock") {
+        computerWins++;
+        return "round: " + round + " " + computerInput + ": Computer wins!";
+      }
+    }
+
+    if (playerSelection === "paper") {
+      if (computerInput === "scissors") {
+        computerWins++;
+        return "round: " + round + " " + computerInput + ": Computer wins!";
+      }
+
+      if (computerInput === "rock") {
+        playerWins++;
+        return "round: " + round + " " + computerInput + ": You win!";
+      }
+    }
+  }
+}
+
+console.log(game());
