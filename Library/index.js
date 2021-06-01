f313768246b23a64e326c107e107438400be4341
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-06-01 15:52:27 +0900

    Update index.js

--- Library/index.js
+++ Library/index.js
@@ -4,13 +4,10 @@ formFields = document.getElementsByClassName('formField');
 block = document.getElementById('block');
 bookForm = document.getElementById('bookForm');
 radio = document.getElementsByClassName('radio');
+bookContain = document.getElementById('bookContain');
+deleteButtons = document.getElementsByClassName('deleteButtons');
 
-for (let i = 0;i < radio.length; i++)
-{
-radio[i].addEventListener('click', () => {
-   radio[i].classList.add('formField');
-})
-}
+
 
 let myLibrary = [];
 
@@ -30,12 +27,13 @@ function addBookToLibrary() {
         bookProperties.push(formFields[i].value);
     }
 
-    console.log(...bookProperties);
+  let book = new Book(...bookProperties);
 
-   var book = new Book(...bookProperties);
     myLibrary.push(book)
-
     console.log(book);
+    console.log(bookProperties);
+    return book;
+
     
 }
 
@@ -57,18 +55,51 @@ block.addEventListener('click', () => {
     block.style.display = "none";
     bookForm.style.display = "none";
 
+    for (let i = 0;i < radio.length; i++)
+    {
+        radio[i].checked = false;
+        radio[i].classList.remove('formField');
+    }
+
     for(let i = 0; i < formFields.length; i++)
     {
         formFields[i].value = "";
     }
-    for (let i = 0;i < radio.length; i++)
-    {
-        radio[i].checked = false;
-    }
+   
 
 })
 
 addConfirm.addEventListener('click', () => 
 {
     addBookToLibrary();
+    
+
+    for (let i = myLibrary.length - 1; i < myLibrary.length; i++)
+    {
+        bookContain.innerHTML += 
+        '<div class="books" id="book' + i + '" data-book = "'+ i +'"><p class="inputs bookTitle">Title: ' + myLibrary[i].title + '</p><p class=" inputs bookAuthor"> Author: ' + myLibrary[i].author + '</p><p class="inputs bookPages">Pages: ' + myLibrary[i].pages + '</p><p class="inputs bookRead">Read: ' + myLibrary[i].read + '</p><p class="inputs bookEdition">Edition: ' + myLibrary[i].edition + '</p><button id="delete-button' + i + '" class="deleteButtons" data-book="' + i + '">Delete</button></div>'
+}
+
+for (let i = 0; i < deleteButtons.length; i++)
+{
+    book = document.getElementById('book' + i)
+deleteButtons[i].addEventListener('click', () => {
+   book.parentNode.removeChild(book);
+   myLibrary.splice(i, 1);
+   deleteButtons.length = deleteButtons.length - 1;
+});
+    }
+
+    block.click();
+
+
 })
+
+for (let i = 0;i < radio.length; i++)
+{
+radio[i].addEventListener('click', () => {
+   radio[i].classList.add('formField');
+})
+}
+
+
